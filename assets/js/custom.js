0a42cbc2b325f05852ac114f2b32683e6cae0cf8
Marc Torres Jimenez <mtorres.form@gmail.com>
2020-06-21 17:28:26 +0200

    More photos for random background

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,6 +1,6 @@
 var pixStyle = "linear-gradient(to top, rgba(46, 49, 65, 0.8), rgba(46, 49, 65, 0.8)), ";
 
-var pics = 17;
+var pics = 24;
 
 function choosePic() {
     var randomNum = Math.floor(Math.random() * pics);
