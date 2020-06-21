f65a34b60490e8566594a6dd77d2ef78aa73bd6a
Marc Torres Jimenez <mtorres.form@gmail.com>
2020-06-21 14:57:19 +0200

    Initial Commit
    Index page mostly done

---
+++ assets/js/custom.js
@@ -0,0 +1,8 @@
+var pixStyle = "linear-gradient(to top, rgba(46, 49, 65, 0.8), rgba(46, 49, 65, 0.8)), ";
+
+var pics = 17;
+
+function choosePic() {
+    var randomNum = Math.floor(Math.random() * pics);
+    document.body.style.backgroundImage = pixStyle + "url('../../rndImages/" + randomNum + ".jpg')";
+}
