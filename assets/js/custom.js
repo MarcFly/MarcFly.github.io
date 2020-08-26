1bd1709672109203dd9a540dac4178107999514f
Marc Torres Jimenez <mtorres.form@gmail.com>
2020-08-26 15:53:43 +0200

    Prepared other Pages
    Project List pages is just getting the list of projects I want to show
    
    Will have a pblog post page that will go indepth in some things I want to (tfg and other things I test and find interesting)
    
    Finally Have to add a way to query instagram for posts according to the photography page.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -6,3 +6,8 @@ function choosePic() {
     var randomNum = Math.floor(Math.random() * pics);
     document.body.style.backgroundImage = pixStyle + "url('../../rndImages/" + randomNum + ".jpg')";
 }
+
+// Test Instagram query
+// Objective is to Query the last 4 instagram posts
+// Get the Main image on each and the description
+// Put them in the html objects
