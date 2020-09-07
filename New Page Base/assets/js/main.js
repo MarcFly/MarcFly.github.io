d4bd6891ced94d3bfad2e39bb34b9a73004cb6ff
Marc Torres Jimenez <mtorres.form@gmail.com>
2020-09-07 13:28:00 +0200

    New page base
    Looking at tutorials and doing from scratch everything
    
    For now, banner and Side Menu, need to include the scrollable header and menu in th eright

---
+++ New Page Base/assets/js/main.js
@@ -0,0 +1,16 @@
+
+// This is a local construction to make things from inside unavailable outside
+
+
+function openMenu(){
+    document.getElementById("menu").style.right="0%";
+
+}
+
+function closeMenu(){
+    document.getElementById("menu").style.right="-20%";
+}
+
+
+
+
