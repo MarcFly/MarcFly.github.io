2060677775a0722314cb36cfdd721ff248391efa
Marc Torres Jimenez <mtorres.form@gmail.com>
2020-09-08 14:01:43 +0200

    Redid side menu
    Solved issue with repeating background
    Hide Header in progress, as I am scrolling #page instead of html itself I can't get scrolling data properly, have to figure it out

--- New Page Base/assets/js/main.js
+++ New Page Base/assets/js/main.js
@@ -1,10 +1,14 @@
 
 // This is a local construction to make things from inside unavailable outside
 
+var menuV = false;
 
-function openMenu(){
-    document.getElementById("menu").style.right="0%";
-
+function toggleMenu(){
+    if(menuV) document.getElementById("menu").style.right="-1000px";
+        
+    else document.getElementById("menu").style.right="0px";
+    
+    menuV = !menuV;
 }
 
 function closeMenu(){
@@ -16,6 +20,25 @@ function chooseBGPic(){
     var pixStyle = "linear-gradient(rgba(46, 41, 87, 0.8) 0%, rgba(46, 41, 87, 0.6) 90%, rgba(46, 41, 87, 0.5) 100%), ";
     document.body.style.background = pixStyle + "url('assets/BGImages/" + randomNum + ".jpg')";
     document.body.style.backgroundSize = "cover";
+}
+
+var prev_perc = 0;
+window.onscroll = function(){
+    var page = document.getElementById("page");
+    var perc = page.scrollTop - document.getElementById("banner").offsetHeight;
+    var el = document.getElementById("hide_head");
+    if (perc > 0 && prev_perc < 0)
+    { 
+        el.style.pointerEvents = "auto";
+        el.style.opacity = "100%";
+    }
+    else if (perc < 0 && prev_perc > 0)
+    {
+        el.style.pointerEvents = "none";
+        el.style.opacity = "0%";
+    }
+    prev_perc = perc;
+
 }
 
 
