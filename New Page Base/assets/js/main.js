75a8ebd7ee60d5446bf1cb70585147f8ed3b951c
Marc Torres Jimenez <mtorres.form@gmail.com>
2020-09-08 11:41:04 +0200

    Background Random Pic with proper sizing

--- New Page Base/assets/js/main.js
+++ New Page Base/assets/js/main.js
@@ -11,6 +11,13 @@ function closeMenu(){
     document.getElementById("menu").style.right="-20%";
 }
 
+function chooseBGPic(){
+    var randomNum = Math.floor(Math.random() * 24);
+    var pixStyle = "linear-gradient(rgba(46, 41, 87, 0.8) 0%, rgba(46, 41, 87, 0.6) 90%, rgba(46, 41, 87, 0.5) 100%), ";
+    document.body.style.background = pixStyle + "url('assets/BGImages/" + randomNum + ".jpg')";
+    document.body.style.backgroundSize = "cover";
+}
+
 
 
 
