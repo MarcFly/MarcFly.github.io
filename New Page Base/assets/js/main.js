f7a2642b9c5c2f0d8a5b9823040fd3e748bc18c7
Marc Torres Jimenez <mtorres.form@gmail.com>
2020-09-10 11:08:42 +0200

    Hiding Header
    No background Image for now, the workarounds fucked up other things and actually does not seem to be clean enough to be useful later.
    
    Now to other parts

--- New Page Base/assets/js/main.js
+++ New Page Base/assets/js/main.js
@@ -4,40 +4,40 @@
 var menuV = false;
 
 function toggleMenu(){
-    if(menuV) document.getElementById("menu").style.right="-1000px";
+    if(menuV)
+    {
+        document.getElementById("menuBTN").style.transform="rotate(180deg)";
+        document.getElementById("menu").style.right="-1000px";
+    }
         
-    else document.getElementById("menu").style.right="0px";
+    else {
+        document.getElementById("menu").style.right="0px";
+        document.getElementById("menuBTN").style.transform="rotate(0deg)";
+    }
     
     menuV = !menuV;
+
+    
 }
 
-function closeMenu(){
-    document.getElementById("menu").style.right="-20%";
-}
+var prev = window.pageYOffset;
 
-function chooseBGPic(){
-    var randomNum = Math.floor(Math.random() * 24);
-    var pixStyle = "linear-gradient(rgba(46, 41, 87, 0.8) 0%, rgba(46, 41, 87, 0.6) 90%, rgba(46, 41, 87, 0.5) 100%), ";
-    document.body.style.background = pixStyle + "url('assets/BGImages/" + randomNum + ".jpg')";
-    document.body.style.backgroundSize = "cover";
-}
-
-var prev_perc = 0;
 window.onscroll = function(){
     var page = document.getElementById("page");
-    var perc = page.scrollTop - document.getElementById("banner").offsetHeight;
-    var el = document.getElementById("hide_head");
-    if (perc > 0 && prev_perc < 0)
+    var bH = document.getElementById("banner").offsetHeight;
+    var curr = window.pageYOffset;
+    var el = document.getElementById("head");
+    if (curr > bH && prev < bH)
     { 
         el.style.pointerEvents = "auto";
         el.style.opacity = "100%";
     }
-    else if (perc < 0 && prev_perc > 0)
+    else if (curr < bH && prev > bH)
     {
         el.style.pointerEvents = "none";
         el.style.opacity = "0%";
     }
-    prev_perc = perc;
+    prev = curr;
 
 }
 
